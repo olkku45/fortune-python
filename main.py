cb8337347af49b3d31a4b6f2921e659f432b32f9
olkku45 <olli.tervonen05@gmail.com>
2025-03-24 19:18:41 +0200

    make working fortune

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+with open("quotes.txt", "r") as file:
+    w_newline = [line.rstrip('\n') for line in file]
+
+
+def split_file_into_chunks(file): 
+    quotes = [""]
+
+    for line in file:
+        if line != "%":
+            if quotes[-1]:
+                quotes[-1] += " "
+            quotes[-1] += line
+        
+        else:
+            quotes.append("")
+
+    return quotes
+
+quotes = split_file_into_chunks(w_newline)  
+
+def get_random_quote(quotes):    
+    quote_index = random.randint(1, len(quotes) + 1)
+
+    return quotes[quote_index]
+
+
+if __name__ == "__main__":
+    print(get_random_quote(quotes)) 
